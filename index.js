18dc7908756af8f0566518ddbd685a58928dbfc6
Adrian Wong <adrianwongapple@gmail.com>
2024-02-02 23:08:29 +0800

    Added the add, subtract, multiply, divide, and operate function

---
+++ index.js
@@ -0,0 +1,34 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+const operate = (operation, num1, num2) => {
+  switch (operation) {
+    case "+":
+      return add(num1, num2);
+
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+
+    default:
+      return null;
+  }
+};
