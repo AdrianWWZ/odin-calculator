1985a8a7136839aea9b64368d9691c27be15a1b6
Adrian Wong <adrianwongapple@gmail.com>
2024-02-02 23:37:23 +0800

    added the initial html for the calculator

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-
-const subtract = (num1, num2) => {
-  return num1 - num2;
-};
-
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
-
-const divide = (num1, num2) => {
-  return num1 / num2;
-};
-
 const operate = (operation, num1, num2) => {
   switch (operation) {
     case "+":
-      return add(num1, num2);
+      return Math.round((num1 + num2) * 10) / 10;
 
     case "-":
-      return subtract(num1, num2);
+      return Math.round((num1 - num2) * 10) / 10;
 
     case "*":
-      return multiply(num1, num2);
+      return Math.round(num1 * num2 * 10) / 10;
 
     case "/":
-      return divide(num1, num2);
+      return Math.round((num1 / num2) * 10) / 10;
 
     default:
       return null;
   }
 };
+
+const display = document.querySelector(".display");
