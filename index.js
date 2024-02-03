44c69bc0e28c7ea40daeb1be6afea88302620f57
Adrian Wong <adrianwongapple@gmail.com>
2024-02-03 18:56:08 +0800

    Finished the calculate part for = for the calculator

--- index.js
+++ index.js
@@ -18,3 +18,60 @@ const operate = (operation, num1, num2) => {
 };
 
 const display = document.querySelector(".display");
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    display.innerText += e.target.innerText;
+  });
+});
+
+const operations = document.querySelectorAll(".operation");
+operations.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    display.innerText += e.target.innerText;
+  });
+});
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  display.innerText = "";
+});
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+  let calculation = display.innerText.match(/\d+\.\d+|\d+|[^0-9]/g);
+
+  while (calculation[1]) {
+    for (let i = 0; i < calculation.length; i++) {
+      if (isNaN(calculation[i])) {
+        if (isNaN(calculation[i - 1]) || isNaN(calculation[i + 1])) {
+          display.innerText = "ERROR";
+          calculation = [];
+        }
+
+        if (calculation[i] === "*") {
+          calculation = calculate(calculation, i);
+          break;
+        } else if (calculation[i] === "/") {
+          calculation = calculate(calculation, i);
+          break;
+        } else if (calculation[i] === "+") {
+          calculation = calculate(calculation, i);
+          break;
+        } else if (calculation[i] === "-") {
+          calculation = calculate(calculation, i);
+          break;
+        }
+      }
+    }
+  }
+  display.innerText = calculation[0];
+});
+
+const calculate = (arr, i) => {
+  const result = operate(arr[i], parseInt(arr[i - 1]), parseInt(arr[i + 1]));
+  arr[i - 1] = result.toString();
+  arr.splice(i, 2);
+  return arr;
+};
